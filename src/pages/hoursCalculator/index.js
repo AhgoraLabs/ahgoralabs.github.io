b24edb4fc76be8e7e9104ffb318edede9a49a2fa
Pedro Verani <pedropverani@hotmail.com>
2023-07-16 09:06:33 -0300

    adding ads

--- src/pages/hoursCalculator/index.js
+++ src/pages/hoursCalculator/index.js
@@ -30,8 +30,10 @@ function HoursCalculator() {
             <h1>Calculadora de Horas</h1>
             <Modal>
                 <div id="div-addNewButtons">
-                    <Button style={{ backgroundColor: "#a2cf6e", color: "white", width: 147 }} onClick={handleAddNewInputs} variant="contained">+</Button>
-                    <Button style={{ backgroundColor: "tomato", color: "white" }} onClick={handleRemoveLastInput} variant="contained">Limpar campos</Button>
+                <Button style={{ backgroundColor: "#a2cf6e", color: "white", minWidth: 147 }} onClick={handleAddNewInputs} variant="contained">+</Button>
+<Button style={{ backgroundColor: "tomato", color: "white", minWidth: 147, fontSize: '0.8rem' }} onClick={handleRemoveLastInput} variant="contained">Limpar campos</Button>
+
+
                 </div>
                 <div id="div-all-input-hours">
                     {new Array(numberOfInputs).fill(0).map((elem, index) => {
