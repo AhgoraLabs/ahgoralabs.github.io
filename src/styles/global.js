344b8e2455b9c44ef1e86f0195d6a581781b829b
Pedro Verani <pedropverani@hotmail.com>
2023-07-17 15:13:29 -0300

    adding local font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from "styled-components";
+import roboto from "../fonts/Roboto/Roboto-Regular.ttf";
 
 export default createGlobalStyle`
+
+@font-face {
+     font-family: 'Roboto';
+     src: url('../fonts/Roboto/Roboto-Regular.ttf') format('truetype');
+     font-weight: 400;
+     font-style: normal;
+   }
+
      * {
           margin:0;
           padding:0;
